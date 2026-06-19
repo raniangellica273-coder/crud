0d8ad41443118b7d0a3f6a15c0c3639a54225dca
raniangellica273-coder <raniangellica273@gmail.com>
2026-06-19 10:25:50 +0700

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+function Home() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/users') 
+        .then(res => setData(res.data))
+        .catch(err => console.log(err));
+    }, [])
+
+  return (
+    <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
+      <h1>List of Users</h1>
+      <div className='rounded bg-white border shadow p-4'>
+        <div className='d-flex justify-content-end'><Link to="/create" className='btn btn-success'>Add</Link></div>
+        <table className='table table-stripend'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data.map((d, i) => (
+                        <tr key={i}>
+                            <td>{d.id}</td>
+                            <td>{d.name}</td>
+                            <td>{d.email}</td>
+                            <td>{d.phone}</td>
+                            <td>
+                                <Link to={`/read/${d.id}`} className='btn btn-sm btn-info me-2 text-white'>Read</Link>
+                                <button className='btn btn-sm btn-primary me-2'>Edit</button>
+                                <button className='btn btn-sm btn-danger'>Delete</button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default Home
